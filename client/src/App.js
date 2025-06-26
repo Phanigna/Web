6cdce0a2952990bfc40cb714b737278a5cac8c2c
Phanigna <125958054+Phanigna@users.noreply.github.com>
2025-06-26 20:18:46 +0530

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+function App() {
+  return (
+    <div style={{ backgroundColor: '#111', minHeight: '100vh', padding: '2rem' }}>
+      <h1 style={{ color: '#f54291' }}>🎬 Welcome to CharanFlix</h1>
+      <p style={{ color: '#ccc' }}>Your movie world, dark and beautiful!</p>
+    </div>
+  );
+}
+
+export default App;
