c4feef98345e8a402c0329aa7a6cb47a40ed442b
Phanigna <125958054+Phanigna@users.noreply.github.com>
2025-06-26 21:04:38 +0530

    Add files via upload

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -1,1 +1,23 @@
+const Movie = require('../models/movieModel');
 
+const getMovies = async (req, res) => {
+  try {
+    const movies = await Movie.find();
+    res.json(movies);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const addMovie = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const newMovie = new Movie({ title, description });
+    await newMovie.save();
+    res.status(201).json(newMovie);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+module.exports = { getMovies, addMovie };
