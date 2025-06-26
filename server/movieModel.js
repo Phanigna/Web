2222e7986508f4ba25014fcc6aa26bf9fece7c74
Phanigna <125958054+Phanigna@users.noreply.github.com>
2025-06-26 21:08:04 +0530

    Add files via upload

--- server/movieModel.js
+++ server/movieModel.js
@@ -1,1 +1,8 @@
+const mongoose = require('mongoose');
 
+const movieSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String
+}, { timestamps: true });
+
+module.exports = mongoose.model('Movie', movieSchema);
