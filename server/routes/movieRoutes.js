252e67dc66f70b9d6ceaf37021032a91e665d696
Phanigna <125958054+Phanigna@users.noreply.github.com>
2025-06-26 21:06:36 +0530

    Add files via upload

--- server/routes/movieRoutes.js
+++ server/routes/movieRoutes.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { getMovies, addMovie } = require('../controllers/movieController');
 
+router.get('/', getMovies);
+router.post('/', addMovie);
+
+module.exports = router;
